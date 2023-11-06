ef7cac9e109afe0e6f0a88b442d0593df6ae9435
Ruslan Albakov <albakov.ruslan@gmail.com>
2023-11-06 15:25:29 +0300

    Добавил описание

--- Geekbrains/hw_les_02/views.py
+++ Geekbrains/hw_les_02/views.py
@@ -5,16 +5,16 @@ from datetime import datetime, timedelta
 
 def customer_prod(request, customer_id, order_delta):
     customer = get_object_or_404(Customer, pk=customer_id)
-    delta_or = datetime.today()-timedelta(days=order_delta)
-    orders = Order.objects.filter(customer=customer).filter(date_ordered__gt=delta_or)
-    pr = []
+    delta_or = datetime.today()-timedelta(days=order_delta) # задаем с какого периода (даты) нам получить данные
+    orders = Order.objects.filter(customer=customer).filter(date_ordered__gt=delta_or) # первый фильтр получает пользователя, второй - спецификатор позволяет найти объекты начиная с периода заданного выше 
+    temp_prod = [] # по заданию нам нужно чтобы "Товары в списке не должны повторятся" поэтому применяю блок с 10 по 14 строку
     for order in orders:
         for product in order.products.all():
-            if product not in pr:
-                pr.append(product)
+            if product not in temp_prod:
+                temp_prod.append(product)
     context = {"title": f"Заказы {customer.name}",
                "customer": customer,
                "orders": orders,
-               "products": pr,
+               "products": temp_prod,
                "delta_or": delta_or}
     return render(request, "hw_les_02/index.html", context)
